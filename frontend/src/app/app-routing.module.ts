83bd4c8c3d0259fb8f4c0c6c7c4b5b69269ede8e
Akif Akkaya <akifakkaya@marun.edu.tr>
2023-11-06 20:16:35 +0300

    dashboard removed

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,20 +1,6 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {DashboardComponent} from './dashboard/dashboard.component';
 import {LoginComponent} from './login/login.component';
-import {AuthGuard} from './guard/auth.guard';
-import {SuperAdminGuard} from './guard/super-admin-guard.service';
-import {UserComponent} from "./dashboard/user/user.component";
-import {ListingUserComponent} from "./dashboard/user/listing-user/listing-user.component";
-import {ListingRoleComponent} from "./dashboard/user/listing-role/listing-role.component";
-import {CustomerComponent} from "./dashboard/customer/customer.component";
-import {ListingCustomerComponent} from "./dashboard/customer/listing-customer/listing-customer.component";
-import {VendorComponent} from "./dashboard/vendor/vendor.component";
-import {ListingVendorComponent} from "./dashboard/vendor/listing-vendor/listing-vendor.component";
-import {WorkingAreaComponent} from "./dashboard/working-area/working-area.component";
-import {
-    ListingWorkingAreaComponent
-} from "./dashboard/working-area/listing-working-area/listing-working-area.component";
 import {HomeComponent} from "./home/home.component";
 import {MovieComponent} from "./detail/movie/movie.component";
 
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,21 +4,11 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {DashboardComponent} from './dashboard/dashboard.component';
 import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 import {ReactiveFormsModule} from '@angular/forms';
 import {LoginComponent} from './login/login.component'
 import {TokenInterceptor} from './service/token.interceptor.';
-import {UserComponent} from './dashboard/user/user.component';
-import {ListingUserComponent} from './dashboard/user/listing-user/listing-user.component';
 import {ErrorInterceptor} from "./service/error.interceptor";
-import { ListingRoleComponent } from './dashboard/user/listing-role/listing-role.component';
-import { CustomerComponent } from './dashboard/customer/customer.component';
-import { VendorComponent } from './dashboard/vendor/vendor.component';
-import { WorkingAreaComponent } from './dashboard/working-area/working-area.component';
-import { ListingCustomerComponent } from './dashboard/customer/listing-customer/listing-customer.component';
-import { ListingVendorComponent } from './dashboard/vendor/listing-vendor/listing-vendor.component';
-import { ListingWorkingAreaComponent } from './dashboard/working-area/listing-working-area/listing-working-area.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
@@ -32,17 +22,7 @@ import { CategoryCardComponent } from './component/category-card/category-card.c
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent,
     LoginComponent,
-    UserComponent,
-    ListingUserComponent,
-    ListingRoleComponent,
-    CustomerComponent,
-    VendorComponent,
-    WorkingAreaComponent,
-    ListingCustomerComponent,
-    ListingVendorComponent,
-    ListingWorkingAreaComponent,
     HomeComponent,
     HeaderComponent,
     FooterComponent,
